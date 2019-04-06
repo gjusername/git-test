8ba10dd3345c6c1d6f4fd5f6a4e01a916a68b772
gao <gao@qq.com>
2019-04-06 18:21:02 +0800

    把本地添加到git

---
+++ git/src/com/gj/test/Git.java
@@ -0,0 +1,11 @@
+package com.gj.test;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("酸奶");
+        System.out.println("酸奶");
+        System.out.println("酸奶");
+        System.out.println("酸奶");
+        System.out.println("酸奶");
+    }
+}
